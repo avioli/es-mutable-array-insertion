d9e6e9afd913e9893fedf44693a2abf20ea57e30
Evo Stamatov <aviolito@gmail.com>
2014-03-20 08:51:07 +1100

    Initial Commit

---
+++ arrayMerge/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  arrayMerge
+//
+//  Created by Evo Stamatov on 20/03/2014.
+//  Copyright (c) 2014 Ionata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
